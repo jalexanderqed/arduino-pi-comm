9155131d64eb34fb8f506a77202f2f6c5e47e470
John Alexander <jalexander.qed@gmail.com>
2017-03-03 07:51:45 +0000

    Update with encoder eval code

--- arduino_code/comm_code/comm_code.ino
+++ arduino_code/comm_code/comm_code.ino
@@ -1,55 +1,35 @@
 const int ledPin = 13;
-
-
+volatile int counter = 0;
 
 void setup()
-
 {
-
   pinMode(ledPin, OUTPUT);
-
   Serial.begin(9600);
-
+  attachInterrupt(digitalPinToInterrupt(3), count, RISING);
 }
-
-
 
 void loop()
-
 {
-
-  Serial.println("Hello Pi");
-
-  if (Serial.available())
-
+  Serial.print("Count: ");
+  Serial.println(counter);
+/*  if (Serial.available())
   {
-
      flash(Serial.read() - '0');
-
-  }
-
+  }*/
   delay(1000);
-
 }
 
-
-
 void flash(int n)
-
 {
-
   for (int i = 0; i < n; i++)
-
   {
-
     digitalWrite(ledPin, HIGH);
-
     delay(100);
-
     digitalWrite(ledPin, LOW);
-
     delay(100);
-
   }
+}
 
+void count(){
+  counter++;
 }
