fbd191997c719c8e32d313ffe352d094cb3ff63c
John T. Alexander <jalexander.qed@gmail.com>
2017-06-02 16:05:52 -0700

    Got encoder working in test1.

--- arduino_code/test1/test1.ino
+++ arduino_code/test1/test1.ino
@@ -1,20 +1,19 @@
 volatile int counter = 0;
-const int encPin1 = 3;
-const int encPin2 = 0;
+const int encPin1 = 2;
+const int encPin2 = 3;
 const int lMotor1 = 9;
 const int lMotor2 = 8;
 const int lMotorC = 6;
 
 void setup()
 {
-  pinMode(encPin1, OUTPUT);
-  pinMode(encPin2, OUTPUT);
+  pinMode(encPin1, INPUT_PULLUP);
+  pinMode(encPin2, INPUT);
   pinMode(lMotor1, OUTPUT);
   pinMode(lMotor2, OUTPUT);
   pinMode(lMotorC, OUTPUT);
-  pinMode(encPin1, INPUT);
   Serial.begin(9600);
-  attachInterrupt(encPin2, count, RISING);
+  attachInterrupt(digitalPinToInterrupt(encPin1), count, RISING);
 }
 
 void loop()
@@ -35,7 +34,7 @@ void loop()
 }
 
 void count(){
-  if(digitalRead(encPin1)){
+  if(digitalRead(encPin2)){
     counter++;
   }
   else{
